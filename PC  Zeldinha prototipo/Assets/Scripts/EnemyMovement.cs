8f34062fdd93b0e6f9c16b7984ce26083d503f1b
PeldL <gabrielgd341@gmail.com>
2025-03-26 11:12:24 -0300

    Animações do enemy e attack

--- PC  Zeldinha prototipo/Assets/Scripts/EnemyMovement.cs
+++ PC  Zeldinha prototipo/Assets/Scripts/EnemyMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float speed;
-    private bool isChansing;
     private int facingDirection = -1;
-    private EnemyState enemyState;
-    
+    private EnemyState enemyState, newstate;
+
+    public float attackRange = 2;
+
     private Rigidbody2D rb;
     private Transform player;
     private Animator anim;
@@ -16,30 +17,51 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ChangeState(EnemyState.Idle);
+       
         rb = GetComponent<Rigidbody2D>();   
         anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (isChansing == true) 
+        if (enemyState == EnemyState.Chasing)
         {
-            if(player.position.x > transform.position.x && facingDirection == -1|| 
-                player.position.x > transform.position.x && facingDirection == 1) 
+            if (enemyState == EnemyState.Chasing)
             {
-                Flip();
+                Chase();
             }
-            Vector2 direction = (player.position - transform.position).normalized;
-            rb.velocity = direction * speed;
+            else if (enemyState ==EnemyState.Attacking) 
+            { 
+             
+            }
         }
        
     }
 
-    void Flip()
+
+    void Chase()
     {
+        if(Vector2.Distance(transform.position, player.transform.position)  <= attackRange) 
+        {
+            ChangeState(EnemyState.Attacking);  
+        }
+
+       else if (player.position.x < transform.position.x && facingDirection == -1 ||
+            player.position.x > transform.position.x && facingDirection == 1)
+        {
+            Flip();
+        }
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.velocity = direction * speed;
+    }
+
+
+
+    void Flip()
+    {   
         facingDirection *= -1;
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
@@ -55,7 +77,7 @@ public class EnemyMovement : MonoBehaviour
             { 
                 player = collision.transform;
             }
-            isChansing = true;
+            ChangeState(EnemyState.Chasing);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -63,14 +85,28 @@ public class EnemyMovement : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             rb.velocity = Vector2.zero;
-            isChansing = false;
+            ChangeState(EnemyState.Idle);
         }
     }
 
     void ChangeState(EnemyState newstate)
     {
+        
         if (enemyState == EnemyState.Idle)
             anim.SetBool("isIdle", false);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", false);
+        else if (enemyState == EnemyState.Attacking)
+            anim.SetBool("isAttacking", false);
+
+        enemyState = newstate;
+
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", true);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", true);
+        else if (enemyState == EnemyState.Attacking)
+            anim.SetBool("isAttacking", true);
     }
 
 
@@ -83,4 +119,5 @@ public enum EnemyState
 {
     Idle,
     Chasing,
+    Attacking
 }
