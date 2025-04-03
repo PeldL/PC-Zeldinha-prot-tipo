05c07d988969522b024e10f36322a5e0a3072c6a
sofaaraujj <sofiasousasilveira695@gmail.com>
2025-04-03 16:34:22 -0300

    menu e poção do jogador

--- PC  Zeldinha prototipo/Assets/Scripts/Menu.cs
+++ PC  Zeldinha prototipo/Assets/Scripts/Menu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
@@ -16,5 +17,21 @@ public class Menu : MonoBehaviour
 
         Application.Quit();
     }
+    
+    [SerializeField] Slider volumeSlider;
 
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            SetVolume(PlayerPrefs.GetFloat("Volume"));
+            volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("Volume", volume);
+    }
 }
